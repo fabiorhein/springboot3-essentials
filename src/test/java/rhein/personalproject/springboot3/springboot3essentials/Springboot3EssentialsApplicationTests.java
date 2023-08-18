f725709bc72df28e70c08264e6af84301042b6f4
Rhein <rhein@Mac-mini-de-Rhein.local>
2023-08-18 17:08:37 -0300

    Springboot v.3.1.2 from start.spring.io

---
+++ src/test/java/rhein/personalproject/springboot3/springboot3essentials/Springboot3EssentialsApplicationTests.java
@@ -0,0 +1,13 @@
+package rhein.personalproject.springboot3.springboot3essentials;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Springboot3EssentialsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
