9ec006f259fd700d643469e7b956b5b6e1816699
Fabio Rhein <fabiorhein@gmail.com>
2023-08-20 18:12:44 -0300

    Created Get Method for Car List All

--- src/main/java/rhein/personalproject/springboot3/domain/Car.java
+++ src/main/java/rhein/personalproject/springboot3/domain/Car.java
@@ -1,10 +1,18 @@
 package rhein.personalproject.springboot3.domain;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
 public class Car extends Entity {
     private String name;
     private String model;
     private long registerNumber;
     private String colour;
     private int numberOfSeat;
-
 }
--- src/main/java/rhein/personalproject/springboot3/domain/Entity.java
+++ src/main/java/rhein/personalproject/springboot3/domain/Entity.java
@@ -1,7 +1,10 @@
 package rhein.personalproject.springboot3.domain;
 
+import lombok.Data;
+
 import java.util.UUID;
 
+@Data
 public class Entity {
     private UUID id;
 }
