d20e864422431282548532604c7b2ec886383cd0
Fabio Rhein <fabiorhein@gmail.com>
2023-08-20 21:25:04 -0300

    Created GetAll, Save, Delete and Update Methods

--- src/main/java/rhein/personalproject/springboot3/controller/CarController.java
+++ src/main/java/rhein/personalproject/springboot3/controller/CarController.java
@@ -2,11 +2,12 @@ package rhein.personalproject.springboot3.controller;
 
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 import rhein.personalproject.springboot3.domain.Car;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
@@ -15,26 +16,43 @@ import java.util.List;
 @Slf4j
 @RequiredArgsConstructor
 public class CarController {
+    private final List<Car> carList = Arrays.asList(Car.builder()
+            .name("Kombi")
+            .colour("Azul")
+            .model("82")
+            .numberOfSeat(9)
+            .registerNumber(123456789)
+            .build(), Car.builder()
+            .name("Fusca")
+            .colour("Verde")
+            .model("79")
+            .numberOfSeat(4)
+            .registerNumber(987654321)
+            .build());
+
     @GetMapping
-    public List<Car> listAll() {
+    public ResponseEntity<List<Car>> listAll() {
+        return ResponseEntity.ok(carList);
+    }
 
-        Car car1 = Car.builder()
-                .name("Fusca")
-                .colour("Verde")
-                .model("79")
-                .numberOfSeat(4)
-                .registerNumber(123456789)
-                .build();
+    @PostMapping
+    public ResponseEntity<Car> save(@RequestBody Car car) {
+        List<Car> cars =  new ArrayList<>(carList);
+        cars.add(car);
+        return ResponseEntity.ok(car);
+    }
 
-        Car car2 = Car.builder()
-                .name("Fusca")
-                .colour("Verde")
-                .model("79")
-                .numberOfSeat(4)
-                .registerNumber(123456789)
-                .build();
-        List<Car> carList = Arrays.asList(car1, car2);
-        log.info(carList.toString());
-        return carList;
+    @DeleteMapping
+    public ResponseEntity<Void> delete(@RequestBody Car car) {
+        List<Car> cars =  new ArrayList<>(carList);
+        cars.remove(car);
+        return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+    }
+
+    @PutMapping
+    public ResponseEntity<Void> update(@RequestBody Car car) {
+        List<Car> cars =  new ArrayList<>(carList);
+        Car carFounded = cars.stream().filter(x -> x.equals(car)).findFirst().orElseThrow();
+        return new ResponseEntity<>(HttpStatus.NO_CONTENT);
     }
 }
